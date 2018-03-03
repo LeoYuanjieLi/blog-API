dd645e3cd9b1e182f18d0dba14e9d9bb112807bb
Yuanjie Li <leo.liyj@gmail.com>
2018-03-02 20:43:08 -0800

    first commit, need to work on client.js

--- server.js
+++ server.js
@@ -1,70 +1,27 @@
-const uuid = require('uuid');
+const express = require('express');
+const morgan = require('morgan');
 
-// This module provides volatile storage, using a `BlogPost`
-// model. We haven't learned about databases yet, so for now
-// we're using in-memory storage. This means each time the app stops, our storage
-// gets erased.
+const app = express();
 
-// Don't worry too much about how BlogPost is implemented.
-// Our concern in this example is with how the API layer
-// is implemented, and getting it to use an existing model.
+const blogPostRouter = require('./blogRouter');
 
+// log the http layer
+app.use(morgan('common'));
 
-function StorageException(message) {
-   this.message = message;
-   this.name = "StorageException";
-}
+app.use(express.static('public'));
 
-const BlogPosts = {
-  create: function(title, content, author, publishDate) {
-    const post = {
-      id: uuid.v4(),
-      title: title,
-      content: content,
-      author: author,
-      publishDate: publishDate || Date.now()
-    };
-    this.posts.push(post);
-    return post;
-  },
-  get: function(id=null) {
-    // if id passed in, retrieve single post,
-    // otherwise send all posts.
-    if (id !== null) {
-      return this.posts.find(post => post.id === id);
-    }
-    // return posts sorted (descending) by
-    // publish date
-    return this.posts.sort(function(a, b) {
-      return b.publishDate - a.publishDate
-    });
-  },
-  delete: function(id) {
-    const postIndex = this.posts.findIndex(
-      post => post.id === id);
-    if (postIndex > -1) {
-      this.posts.splice(postIndex, 1);
-    }
-  },
-  update: function(updatedPost) {
-    const {id} = updatedPost;
-    const postIndex = this.posts.findIndex(
-      post => post.id === updatedPost.id);
-    if (postIndex === -1) {
-      throw new StorageException(
-        `Can't update item \`${id}\` because doesn't exist.`)
-    }
-    this.posts[postIndex] = Object.assign(
-      this.posts[postIndex], updatedPost);
-    return this.posts[postIndex];
-  }
-};
-
-function createBlogPostsModel() {
-  const storage = Object.create(BlogPosts);
-  storage.posts = [];
-  return storage;
-}
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
 
 
-module.exports = {BlogPosts: createBlogPostsModel()};
+// when requests come into `/shopping-list` or
+// `/recipes`, we'll route them to the express
+// router instances we've imported. Remember,
+// these router instances act as modular, mini-express apps.
+app.use('/blog-posts', blogPostRouter);
+
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
